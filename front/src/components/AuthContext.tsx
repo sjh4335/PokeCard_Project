1b24360d7f8c2ac9083b19a3f669eb6c3bcba309
Seok_JH <junho5550@naver.com>
2026-03-26 01:11:06 +0900

    테스트 용 줄바꿈

--- front/src/components/AuthContext.tsx
+++ front/src/components/AuthContext.tsx
@@ -1,6 +1,10 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import type { ReactNode } from 'react';
 
+
+
+
+
 interface AuthContextType {
   isLoggedIn: boolean;
   login: (token: string) => void;
@@ -8,6 +12,7 @@ interface AuthContextType {
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
